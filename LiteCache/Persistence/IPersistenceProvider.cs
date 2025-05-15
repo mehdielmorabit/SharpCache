0ac9f446339d9f8c1cd372e89d469784cb2f4f89
Mehdi <mehdielmorabit@outlook.com>
2025-05-15 21:32:34 +0100

    Adding IPersistenceProvider interface for futur persistence implementations

---
+++ LiteCache/Persistence/IPersistenceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCache.Persistence
+{
+    public interface IPersistenceProvider<T> : IDisposable, IAsyncDisposable where T : class
+    {
+        void Save(T item);
+        Task SaveAsync(T item);
+        T? Load(string key);
+        Task<T?> LoadAsync(string key);
+        void Remove(string key);
+        Task RemoveAsync(string key);
+        IEnumerable<T> LoadAll();
+        Task<IEnumerable<T>> LoadAllAsync();
+    }
+}
